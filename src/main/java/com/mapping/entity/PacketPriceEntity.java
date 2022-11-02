1a6bd0c9e82801cd4d038559bcef8d49bfadf3db
= <=>
2022-11-02 20:08:41 +0700

    first init

---
+++ src/main/java/com/mapping/entity/PacketPriceEntity.java
@@ -0,0 +1,18 @@
+package com.mapping.entity;
+
+import com.mapping.entity.base.BaseEntityUUID;
+import lombok.Data;
+import lombok.ToString;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "packet_price")
+@Data
+public class PacketPriceEntity extends BaseEntityUUID {
+    private Double nominal;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name="packet_entity_id")
+    @ToString.Exclude
+    private PacketEntity packetEntity;
+}
